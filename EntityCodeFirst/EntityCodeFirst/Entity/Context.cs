67195362a82ac745bc8c1cdc7177079eb8378d44
Erol Dalkılıç <eroldalg@gmail.com>
2022-10-27 12:37:08 +0300

    key

---
+++ EntityCodeFirst/EntityCodeFirst/Entity/Context.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity;
+
+namespace EntityCodeFirst.Entity
+{
+    public class Context: DbContext
+    {
+        public DbSet<Urunler> urunlers { get; set; }
+        //public string UrunAd { get; set; }
+        //public string UrunMarka { get; set; }
+        //public string UrunKategori { get; set; }
+        //public int UrunStok { get; set; }
+    }
+}
