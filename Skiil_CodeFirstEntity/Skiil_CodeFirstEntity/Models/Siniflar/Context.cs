b67959c6c7a1933bb04c6ee0f73045df74e0ca77
Erol Dalkılıç <eroldalg@gmail.com>
2022-11-03 18:41:14 +0300

    web config

---
+++ Skiil_CodeFirstEntity/Skiil_CodeFirstEntity/Models/Siniflar/Context.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+namespace Skiil_CodeFirstEntity.Models.Siniflar
+{
+    public class Context : DbContext
+    {
+        public DbSet<Yetenekler> Yeteneklers { get; set; }
+       
+    }
+}
---
+++ Skiil_CodeFirstEntity/Skiil_CodeFirstEntity/Models/Siniflar/Yetenekler.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Skiil_CodeFirstEntity.Models.Siniflar
+{
+    public class Yetenekler
+    {
+        [Key]
+        public int ID { get; set; }
+        public string ACIKLAMA { get; set; }
+        public byte DEGER { get; set; }
+    }
+}
